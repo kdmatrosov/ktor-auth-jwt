96eec610a79f5ba259f846793a9ffafa532b7fca
Kirill Matrosov <k.matrosov@orbita.center>
2019-03-06 17:33:59 +0300

    update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.scss';
-interface State {}
-interface Props {}
 
-class App extends Component<Props, State> {
+interface State {
+  login: string,
+  password: string,
+}
+
+class App extends Component {
+  state: State = {
+    login: '',
+    password: '',
+  };
+
+  onValueChange = (key: string, value: string) => this.setState({[key]: value});
+
+  startAuth = async () => {
+    
+  };
+
   render() {
+    const {login, password} = this.state;
     return (
       <div className="App">
         <div className="login box">
           <div className="field">
             <label className="label">Логин</label>
             <div className="control">
-              <input className="input" type="text"/>
+              <input className="input" type="text" value={login} onChange={({target: {value}}) => {
+                this.onValueChange('login', value);
+              }}/>
             </div>
           </div>
           <div className="field">
             <label className="label">Пароль</label>
             <div className="control">
-              <input className="input" type="password"/>
+              <input className="input" type="password" value={password} onChange={({target: {value}}) => {
+                this.onValueChange('password', value);
+              }}/>
             </div>
           </div>
           <div className="field">
-            <a className="button is-medium is-fullwidth">Войти</a>
+            <a className="button is-medium is-fullwidth" onClick={this.startAuth}>Войти</a>
           </div>
         </div>
 
